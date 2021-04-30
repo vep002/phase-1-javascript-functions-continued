3e8f443e30e87f193d191cf7d5b791e2398dad55
Victoria <vep002@gmail.com>
2021-04-30 08:36:51 -0500

    commit tree 46b0ce6d94d783b041bc74fd45927cd0251a0017

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun (activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(work='go to the office') {
+    return `This Monday, I will ${work}.`
+}
+ function wrapAdjective(flair='*') {
+    return function (adj='special') {
+        return `You are ${flair}${adj}${flair}!`
+    }
+ }
